13bba7b47de5e14912f6d70095d9ee55600fb8f0
Administrator <wltngus200@gmail.com>
2024-03-20 18:10:58 +0900

    Green_New

---
+++ src/main/java/com/green/practice/MissionRepeat05.java
@@ -0,0 +1,20 @@
+package com.green.practice;
+
+import java.util.Scanner;
+
+public class MissionRepeat05 {
+    public static void main(String[] args){
+        //5. 설문조사를 가정해서 같은 답변을 한 사람을 같은 그룹에 넣는다(.equals활용)
+        //반장선거나 인기투표 사람들 표 양을 .length로 분석해서 누가 제일 큰지 추출!
+
+        Scanner cap=new Scanner(System.in);
+        //학생 수를 스캐너로 받는다
+        int stu=
+        //배열에 반복적으로 이름 넣기
+        System.out.println("1. 윤형원 2. 류우석 3. 표선미 4. 송인영 5. 강힘찬");
+        System.out.print("반장으로 추천하는 사람의 이름을 적어주세요.");
+        for()//첨자 1개씩 반복해 확인 후 재배열!
+
+
+    }
+}
