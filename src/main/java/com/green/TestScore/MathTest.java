c2e541df2e03fb927a669d74ec579700fadcce7d
Administrator <wltngus200@gmail.com>
2024-03-22 18:10:10 +0900

    Green_New

--- src/main/java/com/green/TestScore/MathTest.java
+++ src/main/java/com/green/TestScore/MathTest.java
@@ -4,7 +4,7 @@ public class MathTest {
     public static void main(String[] args){
         int
     }
-    static int star(String[] args){
+    static int star(int (String[] args){
         int i=(5+2+8)*2;
         //star(인수들인듯 ㅠ 그래서 star(인수, 인수, 인수)로 바깥에서 소환
         return i;
