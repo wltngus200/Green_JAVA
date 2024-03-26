7ae9023d5257bbc708dfa5d5f2b0db41096a6715
Administrator <wltngus200@gmail.com>
2024-03-26 10:44:19 +0900

    Green_New

--- src/main/java/com/green/day8/ch4/FlowEx16_3.java
+++ src/main/java/com/green/day8/ch4/FlowEx16_3.java
@@ -2,6 +2,7 @@ package com.green.day8.ch4;
 
 public class FlowEx16_3 {
     public static void main(String[] args){
+        //가로 10개 세로 5개의 별
         for(int i=0; i<5; i++){
             for(int j=0; j<10; j++){
                 System.out.print("*");
