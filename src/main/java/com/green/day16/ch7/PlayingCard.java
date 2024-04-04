a487058438135ecf5745e684a3d69dc8c24e476e
Administrator <wltngus200@gmail.com>
2024-04-04 17:13:59 +0900

    Green_New

--- src/main/java/com/green/day16/ch7/PlayingCard.java
+++ src/main/java/com/green/day16/ch7/PlayingCard.java
@@ -1,7 +1,6 @@
 package com.green.day16.ch7;
 
 public interface PlayingCard {//p.381
-    public interface PlayingCard{
         //public static final을 자동으로 붙임
         int CLOVER=1;//인터페이스에서 멤버변수 잘 안 쓰는 편
         //관계 없는 것을 넣지 말 것!
@@ -10,19 +9,24 @@ public interface PlayingCard {//p.381
         //public abstract이 자동으로 붙음
         String getCardNumber();
     }
-}
 interface DeepPlayingCard extends PlayingCard{
     String getCardKind();
 }
 class PlayCard implements DeepPlayingCard{//에러 :추상메소드가 있으니 오버라이딩 해라는 강제성
-    public String getCardKind(){return null;}
+    @Override
+    public String getCardKind(){
+        return null;
+    }
+    public String getCardNumber() {
+        return null;
+    }
 }
 class Card{
     public static void main(String[] args){
         PlayingCard pc=new PlayCard();
         pc.getCardNumber();
         DeepPlayingCard dpc=(DeepPlayingCard)pc;
-        dbc.getCardKind();
+        dpc.getCardKind();
         System.out.println("-------끝-------");
     }
 }
