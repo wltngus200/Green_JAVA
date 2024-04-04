bdc736e30a461cecf2c540ccaebde1212b0e0751
Administrator <wltngus200@gmail.com>
2024-04-04 17:31:26 +0900

    Green_New

--- src/main/java/com/green/TestScore/BaseBall/BaseBall_String.java
+++ src/main/java/com/green/TestScore/BaseBall/BaseBall_String.java
@@ -8,6 +8,7 @@ public class BaseBall_String {
         String question="123";//문제
 
         //String usernum=scan.nextLine();//사용자가 입력하게
+        //다른 발상 일치하면 아예 빼버려? 스트라이크 나오면 해당 숫자 _처리(당연히 둘 중 한 쪽만)
         String usernum="359";
         int idx=usernum.indexOf(usernum.charAt(0)); //1자리수를 뽑아내고 인덱스값
         System.out.println(idx);
